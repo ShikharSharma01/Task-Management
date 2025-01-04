b253a3f29ab8e8c8197bd8d97b72b98358bf1093
kd2-80129 <shikhar8920@gmail.com>
2025-01-04 21:02:22 +0530

    Response dto added

---
+++ Backend/src/main/java/com/tm/dtos/SignUpRespDto.java
@@ -0,0 +1,14 @@
+package com.tm.dtos;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRespDto {
+
+    String message;
+}
